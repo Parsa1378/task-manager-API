c5028ff7e108b22feba89fc07d1fe4c17c110308
Parsa Poorsistani <poorsistani13@gmail.com>
2022-08-11 01:33:40 +0430

    mongoDB is connected

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const express = require('express');
 const app = express()
 const tasks = require('./routes/tasks');
 const port = 8080;
-
+const connectDB = require('./controller/db/connect');
 
 //middleware
 app.use(express.json());
 
 app.use('/api/v1/tasks/', tasks);
 
-app.listen(port, () => {
-    console.log(`server runing on port ${port}`);
-})
+const start = async() => {
+    try {
+        await connectDB();
+        app.listen(port, () => {
+            console.log(`server runing on port ${port}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
--- controller/db/connect.js
+++ controller/db/connect.js
@@ -1,1 +1,9 @@
-const connectionString = 'mongodb+srv://prxaps:<password>@todoapinode.xyalz5u.mongodb.net/?retryWrites=true&w=majority';
+const mongodb = require('mongoose');
+const connectionString = 'mongodb+srv://prxaps:parsaP1378@todoapinode.xyalz5u.mongodb.net/TodoAPINode?retryWrites=true&w=majority';
+
+const connectDB = (url) => {
+    return mongodb
+        .connect(connectionString)
+};
+
+module.exports = connectDB;
